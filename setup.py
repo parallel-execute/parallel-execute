5ea3c2aedf4fdee234c4cd9663e4087367d3280c
Sahil Pardeshi <sahilrp7@gmail.com>
2019-05-15 16:01:26 +0530

    update readme

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
 import setuptools
 
-with open("README.rst", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="parallel-execute",
-    version="0.0.3",
+    version="0.0.4",
     author="Sahil Pardeshi",
     author_email="sahilrp7@gmail.com",
     description="Python wrappers for easy multiprocessing and threading",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description=open('README.rst').read(),
     url="https://github.com/parallel-execute/parallel-execute",
     packages=setuptools.find_packages(),
     license='MIT License',
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Development Status :: Production/Stable",
         "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         "Operating System :: OS Independent",
     ]
 )
