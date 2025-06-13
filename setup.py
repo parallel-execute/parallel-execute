ce5cd700ed41418039df57837d1e123b6a125c5f
mrunnu <mrunnuk@gmail.com>
2025-06-13 12:48:10 -0700

    update python versions

--- setup.py
+++ setup.py
@@ -21,6 +21,10 @@ setuptools.setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
         "Operating System :: OS Independent",
     ]
 )
